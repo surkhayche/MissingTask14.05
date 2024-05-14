b1c997ebc872a1fdef9c2e1cdd8c6687528a0493
Surkhay <surkhayshikhiyev@gmail.com>
2024-05-15 02:59:19 +0400

    Yesterdays task but just one

---
+++ MissingTask 14.05/MissingTask 14.05/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MissingTask_14._05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number;
+            //int digit = 0;
+
+            //Console.Write("Ededi daxil edin");
+            //number = int.Parse(Console.ReadLine());
+
+            //while (number > 0)
+            //{
+            //    digit++;
+            //    number = number / 10;
+            //}
+            //Console.WriteLine("Ededin mertebesi" + digit); 
+            //Console.ReadLine();
+
+            
+            
+
+            
+
+
+        }
+            
+    }    
+}
